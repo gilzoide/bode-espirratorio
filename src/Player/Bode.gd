01aec992ad23fad81130d0f793e731f3ba1f3833
gilzoide <gilzoide@gmail.com>
2021-02-22 16:58:50 -0300

    Add AnimationPlayer and use animations for espirro

--- src/Player/Bode.gd
+++ src/Player/Bode.gd
@@ -1,12 +1,36 @@
 extends RigidBody2D
 
+export(Vector2) var sneeze_impulse : Vector2 = Vector2.UP
+export(float) var sneeze_torque : float = 30
+export(float) var sneezing_speed_factor : float = 0.1
+
+onready var intensity_gauge = $Visuals/IntensityGauge
+onready var sprite = $Visuals/Sprite
+onready var animation_player = $AnimationPlayer
+var is_sneezing : bool = false
+
 func _ready() -> void:
 	set_process_input(true)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("espirro"):
 		start_sneezing()
+		animation_player.play("ComecaEspirrar")
+	elif event.is_action_released("espirro"):
+		animation_player.play("Espirro")
 
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	if is_sneezing:
+		state.angular_velocity *= sneezing_speed_factor
+		state.linear_velocity *= sneezing_speed_factor
 
 func start_sneezing() -> void:
-	pass
+	is_sneezing = true
+
+func stop_sneezing() -> void:
+	is_sneezing = false
+
+func sneeze() -> void:
+	stop_sneezing()
+	apply_torque_impulse(sneeze_torque * intensity_gauge.angle)
+	apply_central_impulse(sneeze_impulse.rotated(rotation) * intensity_gauge.angle)
--- src/Player/IntensityGauge/IntensityGauge.gd
+++ src/Player/IntensityGauge/IntensityGauge.gd
@@ -1,23 +1,11 @@
 extends CanvasItem
 
-export(float) var duration : float = 1
-export(float) var speed : float = 1
-export(bool) var looping : bool = true
-var time : float = 0
-var value : float = 0
+export(float, 0, 1) var angle : float = 0 setget set_angle, get_angle
+var _angle: float = 0
 
-func _ready() -> void:
-	set_process(true)
+func set_angle(value: float) -> void:
+	_angle = value
+	material.set_shader_param("textured_angle", value)
 
-func _process(delta: float) -> void:
-	time += delta * speed
-	if time > duration or time < 0:
-		if not looping:
-			set_process(false)
-		speed = -speed
-		time = clamp(time, 0, duration)
-	set_angle(value)
-	value = time / duration
-
-func set_angle(angle: float) -> void:
-	material.set_shader_param("textured_angle", angle)
+func get_angle() -> float:
+	return _angle
