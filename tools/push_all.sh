42aa26db89722350fa4bfe525a8bb3439c9d6712
gilzoide <gilzoide@gmail.com>
2021-02-24 00:01:57 -0300

    Add export presets for all platforms and add all of them to push_all.sh

--- tools/push_all.sh
+++ tools/push_all.sh
@@ -3,8 +3,10 @@
 script_root=$(dirname $(realpath $0))
 project_root="$script_root/.."
 
-butler push --if-changed "$project_root/build/BodeEspirratorio_web.zip"          gilzoide/sneezing-goat:web
-#butler push --if-changed "$project_root/build/BodeEspirratorio_windows.zip"      gilzoide/sneezing-goat:win32
-#butler push --if-changed "$project_root/build/BodeEspirratorio_linux_x86.zip"    gilzoide/sneezing-goat:linux32
-#butler push --if-changed "$project_root/build/BodeEspirratorio_linux_x86_64.zip" gilzoide/sneezing-goat:linux64
-#butler push --if-changed "$project_root/build/BodeEspirratorio_osx.zip"          gilzoide/sneezing-goat:osx
+butler push --if-changed "$project_root/build/BodeEspirratorio_web.zip"      gilzoide/sneezing-goat:web
+butler push --if-changed "$project_root/build/BodeEspirratorio_windows.zip"  gilzoide/sneezing-goat:win32
+butler push --if-changed "$project_root/build/BodeEspirratorio_linux32.zip"  gilzoide/sneezing-goat:linux32
+butler push --if-changed "$project_root/build/BodeEspirratorio_linux64.zip"  gilzoide/sneezing-goat:linux64
+butler push --if-changed "$project_root/build/BodeEspirratorio_osx.zip"      gilzoide/sneezing-goat:osx
+butler push --if-changed "$project_root/build/BodeEspirratorio_android.apk"  gilzoide/sneezing-goat:android
+
--- tools/zip_all.sh
+++ tools/zip_all.sh
@@ -4,9 +4,9 @@ script_root=$(dirname $(realpath $0))
 project_root="$script_root/.."
 pushd "$project_root/build"
 
-# zip BodeEspirratorio_windows -j BodeEspirratorio_windows.{exe,pck}
-# zip BodeEspirratorio_linux_x86 -j BodeEspirratorio_linux.{x86,pck}
-# zip BodeEspirratorio_linux_x86_64 -j BodeEspirratoriolinux.{x86_64,pck}
+zip BodeEspirratorio_windows -j BodeEspirratorio_windows.{exe,pck}
+zip BodeEspirratorio_linux32 -j BodeEspirratorio_linux32.{x86,pck}
+zip BodeEspirratorio_linux64 -j BodeEspirratorio_linux64.{x86_64,pck}
 zip BodeEspirratorio_web -j web/*
 
 popd
