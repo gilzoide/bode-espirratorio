668ca20e30b3bbb7a9bb54356647b77522e78511
gilzoide <gilzoide@gmail.com>
2021-02-23 22:30:36 -0300

    Update impulse, use sprite instead of polygon2D for HTML export, WIP add exports

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # bode-espirratorio
+
+## Assets
+- Goat vector art created by sentavio: https://www.freepik.com/free-vector/cool-flat-design-trendy-style-animals-icon-set-flat-zoo-children-wild-farm-domestic-animal-cartoon-collection-cow-bull-sheep-horse-goat-pig-dog-cat-pets-turkey-rabbit-hare-hen-chicken_12648710.htm
+- Cooling icon by Iconpacks: https://iconpacks.net/?utm_source=link-attribution&utm_content=3340
--- src/Player/Bode.gd
+++ src/Player/Bode.gd
@@ -1,13 +1,15 @@
 extends RigidBody2D
 
-export(Vector2) var sneeze_impulse : Vector2 = Vector2.UP
-export(float) var sneeze_torque : float = 30
+export(float) var sneeze_impulse : float = 100
+#export(float) var sneeze_torque : float = 30
 export(float) var sneezing_speed_factor : float = 0.1
 
-onready var intensity_gauge = $Visuals/IntensityGauge
-onready var sprite = $Visuals/Sprite
+onready var sneeze_point = $SneezePoint
+onready var sneeze_direction = $SneezeDirection
+onready var intensity_gauge = $IntensityGauge
 onready var animation_player = $AnimationPlayer
 var is_sneezing : bool = false
+var sneeze_value : float = 0
 
 func _ready() -> void:
 	set_process_input(true)
@@ -16,21 +18,32 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("espirro"):
 		start_sneezing()
 		animation_player.play("ComecaEspirrar")
+		print("ESPIRRA")
 	elif event.is_action_released("espirro"):
+		sneeze_value = intensity_gauge.angle
+		animation_player.seek(0, true)
 		animation_player.play("Espirro")
 
-func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+var previous_angular_velocity
+var previous_linear_velocity
+func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
 	if is_sneezing:
-		state.angular_velocity *= sneezing_speed_factor
-		state.linear_velocity *= sneezing_speed_factor
+		angular_velocity = previous_angular_velocity * sneezing_speed_factor
+		linear_velocity = previous_linear_velocity * sneezing_speed_factor
 
 func start_sneezing() -> void:
+	previous_angular_velocity = angular_velocity
+	previous_linear_velocity = linear_velocity
 	is_sneezing = true
+#	sleeping = true
 
 func stop_sneezing() -> void:
 	is_sneezing = false
+#	angular_velocity = previous_angular_velocity
+#	linear_velocity = previous_linear_velocity
 
 func sneeze() -> void:
 	stop_sneezing()
-	apply_torque_impulse(sneeze_torque * intensity_gauge.angle)
-	apply_central_impulse(sneeze_impulse.rotated(rotation) * intensity_gauge.angle)
+	sleeping = true
+	var impulse = sneeze_value * (sneeze_direction.position - sneeze_point.position).normalized() * sneeze_impulse
+	apply_impulse(transform.basis_xform(sneeze_point.position), transform.basis_xform(impulse))
